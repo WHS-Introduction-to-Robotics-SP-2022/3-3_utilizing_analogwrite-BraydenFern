af542de9e22b625db5cbd111a2048f0240e2822e
BraydenFern <99289692+BraydenFern@users.noreply.github.com>
2022-03-08 12:52:51 -0700

    Create 3.3-Utilizing_analogWrite.ino

---
+++ 3.3-Utilizing_analogWrite.ino
@@ -0,0 +1,44 @@
+int blueLED=9;
+int greenLED=10;
+int redLED=11;
+int OneSec=1000;
+
+
+void setup() {
+  
+  pinMode(blueLED,OUTPUT);
+  pinMode(greenLED,OUTPUT);
+  pinMode(redLED,OUTPUT);
+  
+  analogWrite(blueLED,255);
+  delay(OneSec);
+  
+  analogWrite(blueLED,102);
+  delay(OneSec);
+  
+  analogWrite(blueLED,0);
+  delay(OneSec);
+  
+}
+
+void loop() {
+  
+      analogWrite(greenLED,255);
+  delay(OneSec);
+  
+  analogWrite(greenLED,102);
+  delay(OneSec);
+  
+  analogWrite(greenLED,0);
+  delay(OneSec);
+  
+    analogWrite(redLED,255);
+  delay(OneSec);
+
+  analogWrite(redLED,102);
+  delay(OneSec);
+  
+  analogWrite(redLED,0);
+  delay(OneSec);
+    
+}
